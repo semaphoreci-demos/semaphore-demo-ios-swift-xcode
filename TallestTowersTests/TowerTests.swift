0176a52dced78f88856cbe9c0b34fed38a0d8b06
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-31 17:43:48 +0000

    Copy paste TODO as comments

--- TallestTowersTests/TowerTests.swift
+++ TallestTowersTests/TowerTests.swift
@@ -1,2 +1,10 @@
 import XCTest
 @testable import TallestTowers
+
+//- [ ] Create a test case called 'TowerInstanceTests'
+//    - [ ] Create a a setup function. It should create a test variable to store a dummy tower, use the implicit unwrapped optional.
+//    - [ ] 'testLocationShouldBeCreatedFromLatitudeAndLongitudeProperties' - check you can access the location's latitude and longitude for a Tower instance. Pay attention to accuracy.
+//    - [ ] 'testCityAndCountryShouldConcatenateCityAndCountry' - self-evident.
+//    - [ ] 'testFormattedHeightIncludesUnits' - self-evident.
+//- [ ] Create a test case called 'TowerStaticTests'
+//    - [ ] 'testTallestTowersShouldNotBeEmpty' - self-evident.
--- TallestTowersUITests/TallestTowersUITests.swift
+++ TallestTowersUITests/TallestTowersUITests.swift
@@ -1,1 +1,7 @@
 import XCTest
+
+//- [ ] Create a test case called 'TallestTowersUITests'
+//- [ ] Set up the app to launch
+//- [ ] Check that 'Burj Khalifa' exists in the list and tap on it
+//- [ ] Check that 'Shanghai Tower' exists in the list and tap on it
+//- [ ] Tap on a tower entry and check the details match that tower
