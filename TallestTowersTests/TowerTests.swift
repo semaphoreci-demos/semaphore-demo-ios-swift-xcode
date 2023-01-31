6a3c4287679051f93f0f542622cc213cd8a8e592
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-31 17:46:10 +0000

    Created placeholder test cases

--- TallestTowersTests/TowerTests.swift
+++ TallestTowersTests/TowerTests.swift
@@ -1,10 +1,18 @@
 import XCTest
 @testable import TallestTowers
 
-//- [ ] Create a test case called 'TowerInstanceTests'
+//- [x] Create a test case called 'TowerInstanceTests'
 //    - [ ] Create a a setup function. It should create a test variable to store a dummy tower, use the implicit unwrapped optional.
 //    - [ ] 'testLocationShouldBeCreatedFromLatitudeAndLongitudeProperties' - check you can access the location's latitude and longitude for a Tower instance. Pay attention to accuracy.
 //    - [ ] 'testCityAndCountryShouldConcatenateCityAndCountry' - self-evident.
 //    - [ ] 'testFormattedHeightIncludesUnits' - self-evident.
-//- [ ] Create a test case called 'TowerStaticTests'
+//- [x] Create a test case called 'TowerStaticTests'
 //    - [ ] 'testTallestTowersShouldNotBeEmpty' - self-evident.
+
+class TowerInstanceTests: XCTestCase {
+    // do something
+}
+
+class TowerStaticTests: XCTestCase {
+    // do something
+}
